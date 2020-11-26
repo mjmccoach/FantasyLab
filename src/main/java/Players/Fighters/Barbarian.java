572d79fe89867710035a56bf391556f9eb8b2120
mjmccoach <mmccoach88@gmail.com>
2020-11-26 10:54:46 +0000

    Added Fighter Abstract Class

---
+++ src/main/java/Players/Fighters/Barbarian.java
@@ -0,0 +1,4 @@
+package Players.Fighters;
+
+public class Barbarian {
+}
---
+++ src/main/java/Players/Fighters/Fighter.java
@@ -0,0 +1,12 @@
+package Players.Fighters;
+
+
+import Players.Character;
+
+public abstract class Fighter extends Character {
+
+
+    public Fighter(String name, int health) {
+        super(name, health);
+    }
+}
