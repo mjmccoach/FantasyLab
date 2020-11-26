6bfb228818275b15e2a974a583d2aa721f4624de
mjmccoach <mmccoach88@gmail.com>
2020-11-26 10:44:20 +0000

    Initial commit

---
+++ src/main/java/Players/Character.java
@@ -0,0 +1,27 @@
+package Players;
+
+public abstract class Character {
+    private String name;
+    private int health;
+
+    public Character(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
---
+++ src/test/java/CharacterTest.java
@@ -0,0 +1,38 @@
+import Players.Character;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CharacterTest {
+
+    Character character;
+
+    @Before
+    public void before() {
+        character = new Character("Gandalf",100) {
+        };
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Gandalf", character.getName());
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(100, character.getHealth());
+    }
+
+    @Test
+    public void canSetName() {
+        character.setName("Dumbledore");
+        assertEquals("Dumbledore", character.getName());
+    }
+
+    @Test
+    public void canSetHealth() {
+        character.setHealth(50);
+        assertEquals(50, character.getHealth());
+    }
+}
